868588660eaf5e417ba3ef620415a4e1e1294528
amandaalimere <amanda.mere@aluno.senai.br>
2024-04-03 16:42:03 -0300

    aula de hoje

--- src/components/Navegacao/Navegacao.jsx
+++ src/components/Navegacao/Navegacao.jsx
@@ -24,7 +24,7 @@ function Navegacao() {
                <Link style={{color: "#A7A000"}} to="/depoimentos">Depoimentos</Link>
                 </li>
                 <li>
-               <Link style={{color: "#A7A000"}} to="/Login"></Link>
+               <Link style={{color: "#A7A000"}} to="/login">Login</Link>
                 </li>
              </ul>
            </div>
---
+++ src/pages/Fotos/Fotos.jsx
@@ -0,0 +1,11 @@
+import Navegacao from '../../components/Navegacao/Navegacao';
+function Fotos() {
+    return(
+        <>
+            <Navegacao />
+            <h1><p style={{color:"#000"}}>NADA AQUI AINDA</p></h1>
+        </>
+    );
+}
+
+export default Fotos
